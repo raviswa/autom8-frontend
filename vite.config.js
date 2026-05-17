917a607751933575d427fc57bd3910b5e08b0724
Ravi S Viswanathan <84661559+raviswa@users.noreply.github.com>
2026-05-17 23:16:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@
 // Add this comment to force cache bust
 // build: 2026-05-17
 
-npm install react-chartjs-2 chart.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
