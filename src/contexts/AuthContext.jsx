6ffeaf824c7d942b779d017498f1dde6ceb21ee7
Ravi S Viswanathan <84661559+raviswa@users.noreply.github.com>
2026-05-18 06:39:04 +0530

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,7 +6,7 @@
 import React, { createContext, useState, useCallback, useEffect } from 'react';
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
+export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 );
